5f13ef0ec740ae73b9125d68e04d737c929147e9
codewithgsp <codewithgsp@gmail.com>
2020-06-06 21:09:08 +0200

    CreditCal - fixed interest rate

--- Credit Calculator/task/creditcalc/creditcalc.py
+++ Credit Calculator/task/creditcalc/creditcalc.py
@@ -1,28 +1,81 @@
-# import statement
-from math import ceil
+from math import ceil, log
 
-credit_principal = int(input('Enter the credit principal:\n'))
-option = input("""
-What do you want to calculate? 
-type "m" - for count of months, 
-type "p" - for monthly payment:
-""")
 
-if option == 'm':
-    monthly_payment = int(input('Enter monthly payment:\n'))
-    months_required = int(round(credit_principal/monthly_payment))
-    if months_required == 1:
-        print('It takes {} month to repay the credit'.format(months_required))
+def calc_nominal_interest(ci):
+    return ci / (12 * 100)
+
+
+def calc_count_periods(p, mp, int_):
+    """
+    :param p: principal
+    :param mp: monthly payment
+    :param int_: nominal interest
+    :return: number of months in int
+    """
+    return log((mp / (mp - int_ * p)), 1 + int_)
+
+
+def display_months(month):
+    month = ceil(month)
+    if month // 12 == 0:
+        print('You need {month} months to repay this credit!'.format(month=month))
     else:
-        print('It takes {} months to repay the credit'.format(months_required))
+        year = month // 12
+        month = month % 12
+        if year == 1 and month == 1:
+            print('You need {year} year and {month} month to replay this credit!'.format(year=year,
+                                                                                         month=month))
+        elif year == 1:
+            print('You need {year} year and {month} months to replay this credit!'.format(year=year,
+                                                                                          month=month))
+        elif month == 1:
+            print('You need {year} year and {month} month to replay this credit!'.format(year=year,
+                                                                                         month=month))
+        else:
+            print('You need {year} years and {month} months to replay this credit!'.format(year=year,
+                                                                                           month=month))
+
+
+def calc_principal(mp, int_, n):
+    return mp / ((int_ * pow(1 + int_, n)) / (pow(1 + int_, n) - 1))
+
+
+def calc_monthly_payment(p, n, int_):
+    return p * ((int_ * pow(1 + int_, n)) / (pow(1 + int_, n) - 1))
+
+
+def display_monthly_payment(mp):
+    print('Your annuity payment = {}!'.format(ceil(mp)))
+
+
+def display_principal(p):
+    print('Your credit principal = {}!'.format(int(p)))
+
+
+option = input('''
+What do you want to calculate?
+type "n" - for count of months,
+type "a" - for annuity monthly payment,
+type "p" - for credit principal:
+''')
+if option == 'n':
+    principal = float(input('Enter credit principal:\n'))
+    monthly_payment = float(input('Enter monthly payment:\n'))
+    credit_interest = float(input('Enter credit interest:\n'))
+    i = calc_nominal_interest(credit_interest)
+    months = calc_count_periods(p=principal, mp=monthly_payment, int_=i)
+    display_months(months)
 elif option == 'p':
-    months_required = int(input('Enter count of months:\n'))
-    monthly_payment = int(ceil(credit_principal / months_required))
-    last_payment = int(credit_principal - (months_required - 1) * monthly_payment)
-    if last_payment == monthly_payment:
-        print('Your monthly payment = {}'.format(last_payment))
-    else:
-        print('Your monthly payment = {} with last month payment = {}'.format(monthly_payment,
-                                                                              last_payment))
-
- 
+    monthly_payment = float(input('Enter monthly payment:\n'))
+    count_periods = float(input('Enter count of periods:\n'))
+    credit_interest = float(input('Enter credit interest:\n'))
+    i = calc_nominal_interest(credit_interest)
+    principal = calc_principal(monthly_payment, i, count_periods)
+    display_principal(principal)
+elif option == 'a':
+    principal = float(input('Enter credit principal:\n'))
+    count_periods = float(input('Enter count of periods:\n'))
+    credit_interest = float(input('Enter credit interest:\n'))
+    i = calc_nominal_interest(credit_interest)
+    monthly_payment = calc_monthly_payment(principal, count_periods, i)
+    display_monthly_payment(monthly_payment)
---
+++ Problems/The area of a circle/task.py
@@ -0,0 +1,5 @@
+from math import pi
+
+# calculate the area of the circle
+area = pi * int(input()) ** 2
+print(round(area, 2))
